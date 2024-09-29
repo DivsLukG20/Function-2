b1f292fdeeab76f084152ca435b478a36192b184
DivsLukG20 <divashan.luke@gmail.com>
2024-09-29 22:50:44 +0200

    Second Commit

--- database.js
+++ database.js
@@ -3,16 +3,19 @@
 // Author: Codec
 // Link: https://www.youtube.com/watch?v=tIV90xQ0k6A
 
+// Import the mysql module for creating MySQL connections.
 var mysql = require("mysql2");
 
+// Create a connection to the MySQL database.
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3307',
-    database: 'log_queries',
-    user: 'root',
-    password: '@dvtech123!'
+    host: 'localhost', // The hostname of the MySQL server.
+    port: '3307', // The port number on which the MySQL server is listening.
+    database: 'log_queries', // The name of the database to connect to.
+    user: 'root', // The username for authentication.
+    password: '@dvtech123!' // The password for the user authentication. 
 });
 
+// Export the connection object for use in other modules. 
 module.exports = connection; 
 
 // ______end______
--- index.js
+++ index.js
@@ -3,23 +3,32 @@
 // Author: Codec
 // Link: https://www.youtube.com/watch?v=tIV90xQ0k6A
 
+// Imports for the required modules such as express framework, express application and the database connection. 
 var express = require("express")
 var app = express();
 var connection = require('./database')
 
+// Define a GET endpoint for the root path.
 app.get('/', function(req, res){
+    // SQL query to select the most recent 100 log entries ordered by dateTime.
     let sql = "SELECT * FROM log_info ORDER BY dateTime DESC LIMIT 100";
+    // Handle any errors that occur during the query.
     connection.query(sql, function(err, results){
         if (err) throw err;
+        // Send the results back to the client.
         res.send(results);
     })
 })
 
+// Start the server and listen on port 3000. 
 app.listen(3000, function(){
     console.log('App Listening on port 3000');  
+    // Connect to the database.
     connection.connect(function(err){
+        // Handle any connection errors. 
         if(err) throw err;
         console.log('Database connected!')
     })
 });
+
 // ______end______
